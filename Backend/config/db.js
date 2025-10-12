ae434469fcfeb745db9c77e4d168a100457ab182
Sukrut28 <sukrutchinchalkar1@gmail.com>
2025-10-12 23:10:00 +0530

    Initial commit for Roshara

---
+++ Backend/config/db.js
@@ -0,0 +1,9 @@
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
